8657638ea8276cb6ed6c21bd1aa0296c73711163
prbodmann <prbodmann@gmail.com>
2023-04-03 16:15:08 -0300

    lol

--- ensemble_tpu/create_tf_model.py
+++ ensemble_tpu/create_tf_model.py
@@ -127,7 +127,7 @@ def tflite_converter(model,x_train,name):
     converter_quant.optimizations = [tf.lite.Optimize.DEFAULT]
     converter_quant.representative_dataset = representative_data_gen
     converter_quant.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-    converter_quant.target_spec.supported_types = [tf.uint8]
+    converter_quant.target_spec.supported_types = [tf.int8]
     # Just accept that observations and actions are inherently floaty, let Coral handle that on the CPU
     converter_quant.inference_input_type = tf.int8
     converter_quant.inference_output_type = tf.int8
