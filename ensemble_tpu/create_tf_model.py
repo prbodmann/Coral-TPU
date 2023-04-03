1ed06efa8f4dab5ad480eb940f649a47d37ca823
prbodmann <prbodmann@gmail.com>
2023-04-03 17:08:03 -0300

    lol

--- ensemble_tpu/create_tf_model.py
+++ ensemble_tpu/create_tf_model.py
@@ -25,8 +25,8 @@ NIN_CNN_WEIGHT_FILE = os.path.join(os.getcwd(), 'weights', 'nin_cnn_pretrained_w
 
 def load_data() -> Tuple [np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x_train = x_train / 255
-    x_test = x_test / 255
+    x_train = x_train / 255.
+    x_test = x_test / 255.
     y_train = to_categorical(y_train, num_classes=10)
     return x_train, x_test, y_train, y_test
 
