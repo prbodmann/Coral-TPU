2158393be64ba0d32ea8cc90ac241cd4cdbb12b3
prbodmann <prbodmann@gmail.com>
2023-04-03 16:30:16 -0300

    lol

--- ensemble_tpu/create_tf_model.py
+++ ensemble_tpu/create_tf_model.py
@@ -124,13 +124,13 @@ def tflite_converter(model,x_train,name):
 
     converter_quant = tf.lite.TFLiteConverter.from_keras_model(model)
     #converter_quant.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-    converter_quant.optimizations = [tf.lite.Optimize.DEFAULT]
     converter_quant.representative_dataset = representative_data_gen
-    #converter_quant.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter_quant.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
     converter_quant.target_spec.supported_types = [tf.int8]
+    converter_quant.optimizations = [tf.lite.Optimize.DEFAULT]
     # Just accept that observations and actions are inherently floaty, let Coral handle that on the CPU
-    converter_quant.inference_input_type = tf.float32
-    converter_quant.inference_output_type = tf.float32
+    #converter_quant.inference_input_type = tf.float32
+    #converter_quant.inference_output_type = tf.float32
     conveeted_model = converter_quant.convert()
 
     with open(name, 'wb') as f:
