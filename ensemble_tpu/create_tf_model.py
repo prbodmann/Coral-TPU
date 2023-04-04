84b8ac7c637a9ef1c2f631fe5ee3641ed47c90d6
prbodmann <prbodmann@gmail.com>
2023-04-04 11:02:26 -0300

    lol

--- ensemble_tpu/create_tf_model.py
+++ ensemble_tpu/create_tf_model.py
@@ -117,8 +117,10 @@ def ensemble(models: List [training.Model], model_input: Tensor) -> training.Mod
 def tflite_converter(model,x_train,name):
 
     def representative_data_gen():
-            for i in x_train:
-                yield [i]
+        #print(x_test[0])
+        for x in x_test:
+            data = tf.reshape(x, shape=[-1, 32, 32, 3])
+            yield [tf.cast(data,tf.float32)]
 
     converter_quant = tf.lite.TFLiteConverter.from_keras_model(model)
     converter_quant.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -141,46 +143,34 @@ x_train, x_test, y_train, y_test = load_data()
 print('x_train shape: {} | y_train shape: {}\nx_test shape : {} | y_test shape : {}'.format(x_train.shape, y_train.shape,
                                                                                           x_test.shape, y_test.shape))
 input_shape = x_train[0,:,:,:].shape
-print(x_train[0,:,:,:].dtype)
-print(y_train[0,:].dtype)
+print(input_shape)
+#print(x_train[0,:,:,:].dtype)
+#print(y_train[0,:].dtype)
 model_input = Input(shape=input_shape)
 
 conv_pool_cnn_model = conv_pool_cnn(model_input)
-
-#try:
-#    conv_pool_cnn_weight_file
-#except NameError:
-#    conv_pool_cnn_model.load_weights(CONV_POOL_CNN_WEIGHT_FILE)
-
-#conv_pool_cnn_model.compile()
-#conv_pool_cnn_model.summary()
-#conv_pool_cnn_model.evaluate(x_test,y_test,batch_size=32)
-#conv_pool_cnn_model.summary()
-tflite_converter(conv_pool_cnn_model,x_train,"model1.tflite")
+conv_pool_cnn_model.compile()
+try:
+    conv_pool_cnn_weight_file
+except NameError:
+    conv_pool_cnn_model.load_weights(CONV_POOL_CNN_WEIGHT_FILE)
+#tflite_converter(conv_pool_cnn_model,x_train,"model1.tflite")
 
 all_cnn_model = all_cnn(model_input)
-
+all_cnn_model.compile()
 try:
     all_cnn_weight_file
 except NameError:
     all_cnn_model.load_weights(ALL_CNN_WEIGHT_FILE)
-#all_cnn_model.compile()
-#all_cnn_model.evaluate(x_test,y_test,batch_size=32)
-#all_cnn_model.summary()
-tflite_converter(all_cnn_model,x_train,"model2.tflite")
+#tflite_converter(all_cnn_model,x_train,"model2.tflite")
 
 nin_cnn_model = nin_cnn(model_input)
-
+nin_cnn_model.compile()
 try:
     nin_cnn_weight_file
 except NameError:
     nin_cnn_model.load_weights(NIN_CNN_WEIGHT_FILE)
-#nin_cnn_model.compile()
-#evaluate_error(nin_cnn_model)
-#nin_cnn_model.evaluate(x_test,y_test,batch_size=32)
-#nin_cnn_model.summary()
-
-tflite_converter(nin_cnn_model,x_train,"model3.tflite")
+#tflite_converter(nin_cnn_model,x_train,"model3.tflite")
 
 
 models = [conv_pool_cnn_model, all_cnn_model, nin_cnn_model]
