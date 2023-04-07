e1ce66646c46ec8e1ada715c14d54c403438dc16
prbodmann <prbodmann@gmail.com>
2023-04-07 12:20:10 -0300

    lol

--- ensemble_tpu/adaboost_create.py
+++ ensemble_tpu/adaboost_create.py
@@ -96,7 +96,7 @@ model_input = Input(shape=input_shape)
 
 conv_pool_cnn_model = conv_pool_cnn(model_input)
 conv_pool_cnn_model.load_weights(CONV_POOL_CNN_WEIGHT_FILE)
-model_json = model.to_json()
+model_json = conv_pool_cnn_model.to_json()
 with open('model.json', 'w') as json_file:
     json_file.write(model_json)
 model.save_weights('model.h5')
