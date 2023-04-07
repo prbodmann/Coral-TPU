ceb64fe1f8cd9a342561f45b558a59b98ba44c3d
prbodmann <prbodmann@gmail.com>
2023-04-07 12:22:07 -0300

    lol

--- ensemble_tpu/src/utils.py
+++ ensemble_tpu/src/utils.py
@@ -44,3 +44,75 @@ def load_data() -> Tuple [np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     x_test = x_test.astype('float32')
     return x_train, x_test, y_train, y_test
 
+def load_cnn_model(X_test, y_test):
+	# load json and create model
+	json_file = open('model.json', 'r')
+	loaded_model_json = json_file.read()
+	json_file.close()
+	loaded_model = model_from_json(loaded_model_json)
+	# load weights into new model
+	loaded_model.load_weights("model.h5")
+
+	# evaluate loaded model on test data
+	opt_rms = keras.optimizers.rmsprop(lr=0.001,decay=1e-6)
+	loaded_model.compile(
+		loss='categorical_crossentropy',
+		optimizer=opt_rms,
+		metrics=['accuracy'])
+	'''
+	y_test_ = np_utils.to_categorical(y_test, 10)
+	scores = loaded_model.evaluate(X_test, y_test_, batch_size=128, verbose=1)
+	print('\nTest result: %.3f loss: %.3f\n' % (scores[1]*100,scores[0]))
+	'''
+	return loaded_model
+
+def get_feature_layer(model, data):
+
+	total_layers = len(model.layers)
+
+	fl_index = total_layers-2
+
+	feature_layer_model = keras.Model(
+		inputs=model.input,
+		outputs=model.get_layer(index=fl_index).output)
+
+	feature_layer_output = feature_layer_model.predict(data)
+
+	return feature_layer_output
+
+def xgb_model(X_train, y_train, X_test, y_test):
+
+	dtrain = xgb.DMatrix(
+		X_train,
+		label=y_train
+	)
+
+	dtest = xgb.DMatrix(
+		X_test,
+		label=y_test
+	)
+
+	results = {}
+
+	params = {
+		'max_depth':12,
+		'eta':0.05,
+		'objective':'multi:softprob',
+		'num_class':10,
+		'early_stopping_rounds':10,
+		'eval_metric':'merror'
+	}
+
+	watchlist = [(dtrain, 'train'),(dtest, 'eval')]
+	n_round = 400
+
+	model = xgb.train(
+		params,
+		dtrain,
+		n_round,
+		watchlist,
+		evals_result=results)
+
+	pickle.dump(model, open("cnn_xgboost_final.pickle.dat", "wb"))
+
+	return model
