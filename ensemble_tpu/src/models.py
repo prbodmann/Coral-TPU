c098672a99759cf9dc48327135e1aa34a38d7ab9
prbodmann <prbodmann@gmail.com>
2023-04-10 14:24:34 -0300

    lol

--- ensemble_tpu/src/models.py
+++ ensemble_tpu/src/models.py
@@ -213,7 +213,7 @@ class AdaBoostClassifier(object):
         # There is hidden trouble for classes, here the classes will be sorted.
         # So in boost we have to ensure that the predict results have the same classes sort
 
-        self.classes_ = np.zeros(len(y[0]))
+        self.classes_ = np.zeros((len(y[0],10))
 
         ############for CNN (2):
 #        yl = np.argmax(y)
