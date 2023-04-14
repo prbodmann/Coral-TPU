4def74c7da259248a458fba9e9dc2e925bae6547
prbodmann <prbodmann@gmail.com>
2023-04-14 14:45:51 -0300

    lol

--- ensemble_tpu/src/utils.py
+++ ensemble_tpu/src/utils.py
@@ -41,7 +41,6 @@ def load_data() -> Tuple [np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train = x_train / 255.
     x_test = x_test / 255.
-    y_train = to_categorical(y_train, num_classes=10)
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
     return x_train, x_test, y_train, y_test
