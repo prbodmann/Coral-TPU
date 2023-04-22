1b8c67d0810959fc3b6d993e52d51d5ac29cb7c2
prbodmann <prbodmann@gmail.com>
2023-04-22 11:06:39 -0300

    hah

--- ensemble_tpu/src/models.py
+++ ensemble_tpu/src/models.py
@@ -458,7 +458,7 @@ class AdaBoostClassifier(object):
                 #print(i)
                 for jdx, j in enumerate(i):
                     print(j)
-                    if j < np.finfo(float).eps:
+                    if j == 0.0:
                         proba[idx][jdx]=np.finfo(tf.float32).eps
         #proba[proba < np.finfo(tf.float32).eps] = np.finfo(tf.float32).eps
         log_proba = np.log(proba)
