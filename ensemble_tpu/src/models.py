2d3de9549f54156608cfd7c01a473ef07397c23c
prbodmann <prbodmann@gmail.com>
2023-04-22 11:18:39 -0300

    hah

--- ensemble_tpu/src/models.py
+++ ensemble_tpu/src/models.py
@@ -454,12 +454,7 @@ class AdaBoostClassifier(object):
         # Displace zero probabilities so the log is defined.
         # Also fix negative elements which may occur with
         # negative sample weights.
-        for idx,i in enumerate(proba):
-                #print(i)
-                for jdx, j in enumerate(i):                    
-                    if j < np.finfo(float).eps:
-                        proba[idx][jdx]=np.finfo(float).eps
-        #proba[proba < np.finfo(tf.float32).eps] = np.finfo(tf.float32).eps
+        proba[proba < np.finfo(float).eps] = np.finfo(float).eps
         log_proba = np.log(proba)
         print(log_proba.shape)
         return (n_classes - 1) * (log_proba - (1. / n_classes)
