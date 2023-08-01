60bf93a59dc62b54e3822515f378075a92316979
prbodmann <prbodmann@gmail.com>
2023-08-01 14:44:18 -0300

    lol

--- transformer/cait.py
+++ transformer/cait.py
@@ -96,7 +96,7 @@ The practical implementation of LayerScale is simpler than it might sound.
 """
 
 def igelu(x,a=-0.2888,b=-1.769):
-    return K.tanh(1000.0*x)*(a*(min(x·K.tanh(1000.0*x), -b)+b)^2+1.0)
+    return K.tanh(1000.0*x)*(a*(min(x*K.tanh(1000.0*x), -b)+b)^2+1.0)
 
 
 get_custom_objects().update({'igelu': layers.Activation(igelu)})
