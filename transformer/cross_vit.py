df9565ad2c41a48330b29857bd7280eab8fdca89
prbodmann <prbodmann@gmail.com>
2023-09-12 18:15:14 -0300

    lol

--- transformer/cross_vit.py
+++ transformer/cross_vit.py
@@ -243,7 +243,7 @@ if args.training:
     cait_xxs24_224.fit(
         x=x_train,y= y_train,
         validation_data=(x_test, y_test),
-        epochs=2,
+        epochs=20,
         batch_size=batch_size,
         verbose=1   
     )
