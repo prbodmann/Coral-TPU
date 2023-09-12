c5343790823bf025f508118d434851a6add70420
prbodmann <prbodmann@gmail.com>
2023-09-12 18:12:02 -0300

    lol

--- transformer/cross_vit.py
+++ transformer/cross_vit.py
@@ -34,233 +34,173 @@ from einops import rearrange
 from einops.layers.tensorflow import Rearrange, Reduce
 from math import ceil
 
-def exists(val):
-    return val is not None
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
 
-def default(val, d):
-    return val if exists(val) else d
+class PreNorm(Layer):
+    def __init__(self, fn):
+        super(PreNorm, self).__init__()
 
-def cast_tuple(val, l = 3):
-    val = val if isinstance(val, tuple) else (val,)
-    return (*val, *((val[-1],) * max(l - len(val), 0)))
-
-def always(val):
-    return lambda *args, **kwargs: val
-
-def gelu(x, approximate=False):
-    if approximate:
-        coeff = tf.cast(0.044715, x.dtype)
-        return 0.5 * x * (1.0 + tf.tanh(0.7978845608028654 * (x + coeff * tf.pow(x, 3))))
-    else:
-        return 0.5 * x * (1.0 + tf.math.erf(x / tf.cast(1.4142135623730951, x.dtype)))
-
-class HardSwish(Layer):
-    def __init__(self):
-        super(HardSwish, self).__init__()
+        self.norm = nn.LayerNormalization()
+        self.fn = fn
 
     def call(self, x, training=True):
-        x = x * tf.nn.relu6(x + 3.0) / 6.0
-        return x
-
-class GELU(Layer):
-    def __init__(self, approximate=False):
-        super(GELU, self).__init__()
-        self.approximate = approximate
-
-    def call(self, x, training=True):
-        return gelu(x, self.approximate)
+        return self.fn(self.norm(x), training=training)
 
 class MLP(Layer):
-    def __init__(self, dim, mult, dropout=0.0):
+    def __init__(self, dim, hidden_dim, dropout=0.0):
         super(MLP, self).__init__()
 
-        self.net = [
-            nn.Conv2D(filters=dim * mult, kernel_size=1, strides=1),
-            HardSwish(),
+        def GELU():
+            def gelu(x, approximate=False):
+                if approximate:
+                    coeff = tf.cast(0.044715, x.dtype)
+                    return 0.5 * x * (1.0 + tf.tanh(0.7978845608028654 * (x + coeff * tf.pow(x, 3))))
+                else:
+                    return 0.5 * x * (1.0 + tf.math.erf(x / tf.cast(1.4142135623730951, x.dtype)))
+
+            return nn.Activation(gelu)
+
+        self.net = Sequential([
+            nn.Dense(units=hidden_dim),
+            GELU(),
             nn.Dropout(rate=dropout),
-            nn.Conv2D(filters=dim, kernel_size=1, strides=1),
+            nn.Dense(units=dim),
             nn.Dropout(rate=dropout)
-        ]
-        self.net = Sequential(self.net)
+        ])
 
     def call(self, x, training=True):
         return self.net(x, training=training)
 
 class Attention(Layer):
-    def __init__(self, dim, fmap_size, heads=8, dim_key=32, dim_value=64, dropout=0.0, dim_out=None, downsample=False):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.0):
         super(Attention, self).__init__()
-        inner_dim_key = dim_key * heads
-        inner_dim_value = dim_value * heads
-        dim_out = default(dim_out, dim)
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
 
         self.heads = heads
-        self.scale = dim_key ** -0.5
-
-        self.to_q = Sequential([
-            nn.Conv2D(filters=inner_dim_key, kernel_size=1, strides=(2 if downsample else 1), use_bias=False),
-            nn.BatchNormalization(momentum=0.9, epsilon=1e-05),
-        ])
-
-        self.to_k = Sequential([
-            nn.Conv2D(filters=inner_dim_key, kernel_size=1, strides=1, use_bias=False),
-            nn.BatchNormalization(momentum=0.9, epsilon=1e-05),
-        ])
-
-        self.to_v = Sequential([
-            nn.Conv2D(filters=inner_dim_value, kernel_size=1, strides=1, use_bias=False),
-            nn.BatchNormalization(momentum=0.9, epsilon=1e-05),
-        ])
+        self.scale = dim_head ** -0.5
 
         self.attend = nn.Softmax()
+        self.to_qkv = nn.Dense(units=inner_dim * 3, use_bias=False)
 
-        out_batch_norm = nn.BatchNormalization(momentum=0.9, epsilon=1e-05, gamma_initializer='zeros')
+        if project_out:
+            self.to_out = [
+                nn.Dense(units=dim),
+                nn.Dropout(rate=dropout)
+            ]
+        else:
+            self.to_out = []
 
-        self.to_out = Sequential([
-            GELU(),
-            nn.Conv2D(filters=dim_out, kernel_size=1, strides=1),
-            out_batch_norm,
-            nn.Dropout(rate=dropout)
-        ])
-
-        # positional bias
-        self.pos_bias = nn.Embedding(input_dim=fmap_size * fmap_size, output_dim=heads)
-        q_range = tf.range(0, fmap_size, delta=(2 if downsample else 1))
-        k_range = tf.range(fmap_size)
-
-        q_pos = tf.stack(tf.meshgrid(q_range, q_range, indexing='ij'), axis=-1)
-        k_pos = tf.stack(tf.meshgrid(k_range, k_range, indexing='ij'), axis=-1)
-
-        q_pos, k_pos = map(lambda t: rearrange(t, 'i j c -> (i j) c'), (q_pos, k_pos))
-        rel_pos = tf.abs((q_pos[:, None, ...] - k_pos[None, :, ...]))
-
-        x_rel, y_rel = tf.unstack(rel_pos, axis=-1)
-        self.pos_indices = (x_rel * fmap_size) + y_rel
-
-    def apply_pos_bias(self, fmap):
-        bias = self.pos_bias(self.pos_indices)
-        bias = rearrange(bias, 'i j h -> () h i j')
-        return fmap + (bias / self.scale)
+        self.to_out = Sequential(self.to_out)
 
     def call(self, x, training=True):
-        b, height, width, n = x.shape
-        q = self.to_q(x)
+        qkv = self.to_qkv(x)
+        qkv = tf.split(qkv, num_or_size_splits=3, axis=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
 
-        h = self.heads
-        y = q.shape[1] # height
-
-        qkv = (q, self.to_k(x), self.to_v(x))
-        q, k, v = map(lambda t: rearrange(t, 'b ... (h d) -> b h (...) d', h=h), qkv)
-
-        # i,j = height*width
+        # dots = tf.matmul(q, tf.transpose(k, perm=[0, 1, 3, 2])) * self.scale
         dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
-        dots = self.apply_pos_bias(dots)
-
         attn = self.attend(dots)
 
+        # x = tf.matmul(attn, v)
         x = einsum('b h i j, b h j d -> b h i d', attn, v)
-        x = rearrange(x, 'b h (x y) d -> b x y (h d)', h=h, y=y)
+        x = rearrange(x, 'b h n d -> b n (h d)')
         x = self.to_out(x, training=training)
 
         return x
 
 class Transformer(Layer):
-    def __init__(self, dim, fmap_size, depth, heads, dim_key, dim_value, mlp_mult=2, dropout=0.0, dim_out=None, downsample=False):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.0):
         super(Transformer, self).__init__()
 
-        dim_out = default(dim_out, dim)
-        self.attn_residual = (not downsample) and dim == dim_out
         self.layers = []
 
         for _ in range(depth):
             self.layers.append([
-                Attention(dim, fmap_size=fmap_size, heads=heads, dim_key=dim_key, dim_value=dim_value,
-                          dropout=dropout, downsample=downsample, dim_out=dim_out),
-                MLP(dim_out, mlp_mult, dropout=dropout)
+                PreNorm(Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                PreNorm(MLP(dim, mlp_dim, dropout=dropout))
             ])
 
     def call(self, x, training=True):
         for attn, mlp in self.layers:
-            attn_res = (x if self.attn_residual else 0)
-            x = attn(x, training=training) + attn_res
+            x = attn(x, training=training) + x
             x = mlp(x, training=training) + x
 
         return x
 
-class LeViT(Model):
-    def __init__(self,
-                 image_size,
-                 num_classes,
-                 dim,
-                 depth,
-                 heads,
-                 mlp_mult,
-                 stages=3,
-                 dim_key=32,
-                 dim_value=64,
-                 dropout=0.0,
-                 num_distill_classes=None
-                 ):
-        super(LeViT, self).__init__()
+class ViT(Model):
+    def __init__(self, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim,
+                 pool='cls', dim_head=64, dropout=0.0, emb_dropout=0.0):
+        """
+            image_size: int.
+            -> Image size. If you have rectangular images, make sure your image size is the maximum of the width and height
+            patch_size: int.
+            -> Number of patches. image_size must be divisible by patch_size.
+            -> The number of patches is: n = (image_size // patch_size) ** 2 and n must be greater than 16.
+            num_classes: int.
+            -> Number of classes to classify.
+            dim: int.
+            -> Last dimension of output tensor after linear transformation nn.Linear(..., dim).
+            depth: int.
+            -> Number of Transformer blocks.
+            heads: int.
+            -> Number of heads in Multi-head Attention layer.
+            mlp_dim: int.
+            -> Dimension of the MLP (FeedForward) layer.
+            dropout: float between [0, 1], default 0..
+            -> Dropout rate.
+            emb_dropout: float between [0, 1], default 0.
+            -> Embedding dropout rate.
+            pool: string, either cls token pooling or mean pooling
+        """
+        super(ViT, self).__init__()
 
-        dims = cast_tuple(dim, stages)
-        depths = cast_tuple(depth, stages)
-        layer_heads = cast_tuple(heads, stages)
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
 
-        assert all(map(lambda t: len(t) == stages, (dims, depths, layer_heads))), \
-            'dimensions, depths, and heads must be a tuple that is less than the designated number of stages'
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
 
-        self.conv_embedding = Sequential([
-            nn.Conv2D(filters=32, kernel_size=3, strides=2, padding='SAME'),
-            nn.Conv2D(filters=64, kernel_size=3, strides=2, padding='SAME'),
-            nn.Conv2D(filters=128, kernel_size=3, strides=2, padding='SAME'),
-            nn.Conv2D(filters=dims[0], kernel_size=3, strides=2, padding='SAME')
-        ])
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
-        fmap_size = image_size // (2 ** 4)
-        self.backbone = Sequential()
+        self.patch_embedding = Sequential([
+            Rearrange('b (h p1) (w p2) c -> b (h w) (p1 p2 c)', p1=patch_height, p2=patch_width),
+            nn.Dense(units=dim)
+        ], name='patch_embedding')
 
-        for ind, dim, depth, heads in zip(range(stages), dims, depths, layer_heads):
-            is_last = ind == (stages - 1)
-            self.backbone.add(Transformer(dim, fmap_size, depth, heads, dim_key, dim_value, mlp_mult, dropout))
+        self.pos_embedding = tf.Variable(initial_value=tf.random.normal([1, num_patches + 1, dim]))
+        self.cls_token = tf.Variable(initial_value=tf.random.normal([1, 1, dim]))
+        self.dropout = nn.Dropout(rate=emb_dropout)
 
-            if not is_last:
-                next_dim = dims[ind + 1]
-                self.backbone.add(Transformer(dim, fmap_size, 1, heads * 2, dim_key, dim_value, dim_out=next_dim, downsample=True))
-                fmap_size = ceil(fmap_size / 2)
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
-        self.pool = Sequential([
-            nn.GlobalAvgPool2D()
-        ])
+        self.pool = pool
 
-        self.distill_head = nn.Dense(units=num_distill_classes) if exists(num_distill_classes) else always(None)
-        self.mlp_head = nn.Dense(units=num_classes)
-
+        self.mlp_head = Sequential([
+            nn.LayerNormalization(),
+            nn.Dense(units=num_classes)
+        ], name='mlp_head')
 
     def call(self, img, training=True, **kwargs):
-        x = self.conv_embedding(img)
+        x = self.patch_embedding(img)
+        b, n, d = x.shape
 
-        x = self.backbone(x)
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
+        x = tf.concat([cls_tokens, x], axis=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x, training=training)
 
-        x = self.pool(x)
-        out = self.mlp_head(x)
-        distill = self.distill_head(x)
+        x = self.transformer(x, training=training)
 
-        if exists(distill):
-            return out, distill
+        if self.pool == 'mean':
+            x = tf.reduce_mean(x, axis=1)
+        else:
+            x = x[:, 0]
 
-        return out
-""" Usage
-v = CrossFormer(
-    num_classes = 1000,                # number of output classes
-    dim = (64, 128, 256, 512),         # dimension at each stage
-    depth = (2, 2, 8, 2),              # depth of transformer at each stage
-    global_window_size = (8, 4, 2, 1), # global window sizes at each stage
-    local_window_size = 7,             # local window size (can be customized for each stage, but in paper, held constant at 7 for all stages)
-)
+        x = self.mlp_head(x)
 
-img = tf.random.normal(shape=[1, 224, 224, 3])
-preds = v(img) # (1, 1000)
-"""
+        return x
 
 
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -283,15 +223,16 @@ x_test = x_test / 255.0
 if args.training:
 
 
-    cait_xxs24_224 =  LeViT(
+    cait_xxs24_224 =  ViT(
     image_size = 224,
+    patch_size = 32,
     num_classes = 100,
-    stages = 3,             # number of stages
-    dim = (256, 384, 512),  # dimensions at each stage
-    depth = 4,              # transformer of depth 4 at each stage
-    heads = (4, 6, 8),      # heads at each stage
-    mlp_mult = 2,
-    dropout = 0.1
+    dim = 1024,
+    depth = 6,
+    heads = 16,
+    mlp_dim = 2048,
+    dropout = 0.1,
+    emb_dropout = 0.1   
     )
 
 
