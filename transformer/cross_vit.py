d07a6558fb8bd88b001239fadf2af9ff3c773485
prbodmann <prbodmann@gmail.com>
2023-09-12 17:26:27 -0300

    lol

--- transformer/cross_vit.py
+++ transformer/cross_vit.py
@@ -374,11 +374,11 @@ if args.training:
     
     img = tf.random.normal(shape=[1, 224, 224, 3])
     preds = cait_xxs24_224(img) # (1, 1000)
-    cait_xxs24_224.save('cross_vit.h5')
+    cait_xxs24_224.save('cross_vit',save_format="tf")
     print(results)
     
 else:
-    cait_xxs24_224=  tf.keras.models.load_model('cross_vit.h5')
+    cait_xxs24_224=  tf.keras.models.load_model('cross_vit')
 
 batch_size=1
 def representative_data_gen():
