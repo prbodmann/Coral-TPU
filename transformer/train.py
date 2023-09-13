eb44bf1b5d44905f07de504fa391ff00e4b56c96
prbodmann <prbodmann@gmail.com>
2023-09-13 15:44:47 -0300

    lol

--- transformer/train.py
+++ transformer/train.py
@@ -126,7 +126,7 @@ varied scales. In this example, we discard this part.
 """
 
 train_dataset, val_dataset = tfds.load(
-    "tf_flowers", split=["train[:90%]", "train[90%:]"],     with_info=True,
+    "tf_flowers", split=["train[:90%]", "train[90%:]"], 
     as_supervised=True
 )
 
