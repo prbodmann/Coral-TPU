2075c29e308309b06f1205e14b26782ccddc054d
prbodmann <prbodmann@gmail.com>
2023-09-13 10:49:05 -0300

    lol

--- transformer/teesting.py
+++ transformer/teesting.py
@@ -313,7 +313,7 @@ class ViT(Model):
             nn.Dense(units=num_classes)
         ])
         print(x.shape)
-        _, b, n, _ = x.shape
+        b, n, _ = x.shape[:3]
 
         cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
         x = tf.concat([cls_tokens, x], axis=1)
