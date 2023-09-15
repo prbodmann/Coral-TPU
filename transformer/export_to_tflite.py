5882869b28df440ad7c07cd0212764b2190dbef3
prbodmann <prbodmann@gmail.com>
2023-09-15 18:47:39 -0300

    lol

--- transformer/export_to_tflite.py
+++ transformer/export_to_tflite.py
@@ -86,7 +86,7 @@ def representative_data_gen():
 
 
 
-model = test(args.vit_size, args.num_classes)
+model = viT(args.vit_size, args.num_classes)
 model.load_weights(os.path.join("finetuning_weights", args.source_name)).expect_partial()
 #model.compute_output_shape(input_shape = [1] + VIT_CONFIG[args.vit_size]["image_size"])
 model.build(input_shape=[1] + VIT_CONFIG[args.vit_size]["image_size"])
