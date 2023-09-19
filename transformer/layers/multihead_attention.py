8a7ca40ff2aafa4f766221b25d249245be6f9108
prbodmann <prbodmann@gmail.com>
2023-09-19 16:29:08 -0300

    lol

--- transformer/layers/multihead_attention.py
+++ transformer/layers/multihead_attention.py
@@ -74,8 +74,11 @@ class Multihead_attention(tf.keras.layers.Layer):
         logits = tf.transpose(logits, perm=[0, 2, 1, 3])
         # number_of_heads, head_dimension -> embedding_dimension, concating the logits
         # concatenating to [batch_size, sequence length, num_attention heads * head_dimenson]
-        concated_logits = Reshape(target_shape=(self.embedding_dimension))(logits)
-
+        print("mh1: "+str(logits.shape))
+        concated_logits = tf.reshape(logits,
+                                     (self.batch_size, -1, self.embedding_dimension))
+        print("mh2: "+str(concated_logits.shape))
+        #concated_logits = Reshape(target_shape=(self.embedding_dimension))(logits)
         self.output_logits = self.FFN(concated_logits)
 
         return self.output_logits, attention_weights
