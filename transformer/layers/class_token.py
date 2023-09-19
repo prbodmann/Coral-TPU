c74d51c2857b9f0b853d1d7d87241fb651306dc2
prbodmann <prbodmann@gmail.com>
2023-09-19 17:11:21 -0300

    lol

--- transformer/layers/class_token.py
+++ transformer/layers/class_token.py
@@ -6,13 +6,13 @@ class ClassToken(tf.keras.layers.Layer):
     """
 
     def __init__(self,
-                 embedding_dimension,
+                 embedding_dimension,                 
                  **kwargs):
         super(ClassToken, self).__init__(**kwargs)
         self.embedding_dimension = embedding_dimension
         self.cls_token_tensor = tf.Variable(name="cls",
                                             initial_value = tf.random_normal_initializer(stddev=0.06)
-                                            (shape = (1, 1, self.embedding_dimension)),
+                                            (shape = (None, 1, self.embedding_dimension)),
                                             trainable=True)
 
     def call(self, input_tensor):
