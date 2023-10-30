17a970170aedd5fe1cbc38e7f687a0fdbcaaf1cb
prbodmann <prbodmann@gmail.com>
2023-10-30 13:34:19 -0300

    added stuff

--- transformer/train_convert_imagenet.py
+++ transformer/train_convert_imagenet.py
@@ -5,7 +5,7 @@ from tensorflow.keras.utils import to_categorical
 from nest import NesT
 import tensorflow_datasets as tfds
 
-resize_bigger = 230
+resize_bigger = 300
 image_size = 256
 def preprocess_dataset(is_training=True):
     def _pp(image, label):
@@ -39,10 +39,10 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--training', action = 'store_const', dest = 'training',
                            default = False, required = False,const=True)
 args = parser.parse_args()
-ds = tfds.load('imagenet2012', split=["train[:90%]", "train[90%:]"], as_supervised=True, data_dir='/mnt/dataset', download=True)
+ds = tfds.load('imagenet2012', split=["train[:90%]", "validation[90%:]"], as_supervised=True, data_dir='/mnt/dataset', download=True)
 
-train_dataset = prepare_dataset(train_dataset1, is_training=True,batch_size_=batch_size)
-val_dataset = prepare_dataset(val_dataset1, is_training=False,batch_size_=batch_size)
+train_dataset = prepare_dataset(ds['train'], is_training=True,batch_size_=batch_size)
+val_dataset = prepare_dataset(ds['validation'], is_training=False,batch_size_=batch_size)
 
 if args.training:
 
