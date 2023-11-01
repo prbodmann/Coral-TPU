083503c8ef984cfc10e80e5ce7cc9750979c959c
prbodmann <prbodmann@gmail.com>
2023-11-01 15:59:20 -0300

    added stuff

--- transformer/train_convert.py
+++ transformer/train_convert.py
@@ -31,15 +31,15 @@ def prepare(ds, shuffle=False, augment=False):
               num_parallel_calls=AUTOTUNE)
 
     if shuffle:
-    ds = ds.shuffle(1000)
+        ds = ds.shuffle(1000)
 
     # Batch all datasets.
     ds = ds.batch(batch_size)
 
     # Use data augmentation only on the training set.
     if augment:
-    ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y), 
-                num_parallel_calls=AUTOTUNE)
+        ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y), 
+                    num_parallel_calls=AUTOTUNE)
 
     # Use buffered prefetching on all datasets.
     return ds.prefetch(buffer_size=AUTOTUNE)
