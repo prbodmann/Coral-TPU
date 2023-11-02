0e3200e39ac6b2fbfeadc293b8c6f46fbc40f88e
prbodmann <prbodmann@gmail.com>
2023-11-02 15:37:57 -0300

    added stuff

--- transformer/myvit.py
+++ transformer/myvit.py
@@ -162,7 +162,7 @@ def create_vit_classifier():
         # Layer normalization 1.
         x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
         # Create a multi-head attention layer.
-        attention_output = OptimizedMultiHeadAttention(
+        attention_output = layers.MultiHeadAttention(
             num_heads=num_heads, key_dim=projection_dim, dropout=0.1
         )(x1, x1)
         # Skip connection 1.
