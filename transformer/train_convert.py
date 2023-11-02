ba5be6f9cb5c6573a36c931f2b9f0f69b30ce9a7
prbodmann <prbodmann@gmail.com>
2023-11-02 17:17:25 -0300

    added stuff

--- transformer/train_convert.py
+++ transformer/train_convert.py
@@ -2,7 +2,7 @@ import argparse
 import tensorflow as tf
 from tensorflow.keras import datasets
 from tensorflow.keras.utils import to_categorical
-from mobilevit import MobileViT
+from cait import Cait
 import tensorflow_addons as tfa
 import tensorflow.keras.layers as nn
 
