cfd13ba9edb878b323f53d00a4bef9b883d0cf85
prbodmann <prbodmann@gmail.com>
2023-11-06 16:04:30 -0300

    added stuff

--- transformer/vit.py
+++ transformer/vit.py
@@ -8,6 +8,7 @@ from keras.layers import Activation
 from tensorflow.keras import activations
 import tensorflow_addons as tfa
 import tensorflow.keras.layers as nn
+from tensorflow.keras import Sequential
 
 #0.04553992412
 pi=3.141592653589793
@@ -40,7 +41,7 @@ class CreatePatches( tf.keras.layers.Layer ):
     return  tf.stack(patches,axis=-2)
 
 
-def mlp(x: tf.Tensor, hidden_units: List[int], dropout_rate: float) -> tf.Tensor:
+def mlp( tf.keras.layers.Layer ):
     """Multi-Layer Perceptron
 
     Args:
@@ -50,13 +51,18 @@ def mlp(x: tf.Tensor, hidden_units: List[int], dropout_rate: float) -> tf.Tensor
 
     Returns:
         tf.Tensor: Output
+    
     """
-    print("ol")
-    for units in hidden_units:
-        x = layers.Dense(units,activation=igelu)(x)
-        #x = tf.nn.gelu(x,approximate=True)
-        x = layers.Dropout(dropout_rate)(x)
-    return x
+    def __init__( self , hidden_units,dropout_rate ):
+        super( CreatePatches , self ).__init__()
+        
+        self.net=[]
+        for units in hidden_units:
+            self.net.append(layers.Dense(units, activation=tf.nn.gelu))
+            self.net.append(layers.Dropout(dropout_rate))
+        self.net = Sequential(self.net)
+    def call(self, x, training=True):
+        return self.net(x, training=training)
 
 class Patches2(layers.Layer):
     """Create a a set of image patches from input. The patches all have
@@ -96,6 +102,64 @@ class PatchEncoder(layers.Layer):
         positions = tf.range(start=0, limit=self.num_patches, delta=1)
         encoded = self.projection(patch) + self.position_embedding(positions)
         return encoded
+
+
+
+def create_vit_classifier(input_shape,
+                          num_classes: int,
+                          image_size: int,
+                          patch_size: int,
+                          num_patches: int,
+                          projection_dim: int,
+                          dropout: float,
+                          n_transformer_layers: int,
+                          num_heads: int,
+                          transformer_units: List[int],
+                          mlp_head_units: List[int],
+                          normalization: bool=False):
+    inputs = layers.Input(shape=input_shape)
+    
+
+    augmented = inputs
+    
+    # Create patches.
+    patches = Patches2(patch_size,num_patches,input_image_size=image_size)(augmented)
+    
+    # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(n_transformer_layers):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp( hidden_units=transformer_units, dropout_rate=0.1)(x3)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = layers.Flatten()(representation)
+    representation = layers.Dropout(dropout)(representation)
+    
+    # Add MLP.
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=dropout)
+    
+    # Classify outputs.
+    logits = layers.Dense(num_classes)(features)
+    #print(logits.shape)
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=logits)
+    
+    return model
 
 
 def create_vit_classifier(input_shape,
