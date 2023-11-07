ec8bc295f35cb1dd954d3423e0fc00e23a3cc779
prbodmann <prbodmann@gmail.com>
2023-11-07 11:21:28 -0300

    added stuff

--- transformer/mobilevit.py
+++ transformer/mobilevit.py
@@ -5,6 +5,8 @@ from tensorflow.keras.layers import Layer
 from tensorflow.keras import Sequential
 import tensorflow.keras.layers as nn
 from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras import backend
+
 patch_size = 8
 
 def conv_block(x, filters=16, kernel_size=3, strides=2):
@@ -16,14 +18,37 @@ def conv_block(x, filters=16, kernel_size=3, strides=2):
 
 # Reference: https://git.io/JKgtC
 
+def correct_pad(inputs, kernel_size):
+    """Returns a tuple for zero-padding for 2D convolution with downsampling.
+
+    Args:
+      inputs: Input tensor.
+      kernel_size: An integer or tuple/list of 2 integers.
+
+    Returns:
+      A tuple.
+    """
+    img_dim = 2 if backend.image_data_format() == "channels_first" else 1
+    input_size = inputs.shape[img_dim : (img_dim + 2)]
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size, kernel_size)
+    if input_size[0] is None:
+        adjust = (1, 1)
+    else:
+        adjust = (1 - input_size[0] % 2, 1 - input_size[1] % 2)
+    correct = (kernel_size[0] // 2, kernel_size[1] // 2)
+    return (
+        (correct[0] - adjust[0], correct[0]),
+        (correct[1] - adjust[1], correct[1]),
+    )
 
 def inverted_residual_block(x, expanded_channels, output_channels, strides=1):
     m = layers.Conv2D(expanded_channels, 1, padding="same", use_bias=False)(x)
     m = layers.BatchNormalization()(m)
     m = tf.nn.swish(m)
 
-    #if strides == 2:
-    #    m = layers.ZeroPadding2D(padding=imagenet_utils.correct_pad(m, 3))(m)
+    if strides == 2:
+        m = layers.ZeroPadding2D(padding=correct_pad(m, 3))(m)
     m = layers.DepthwiseConv2D(
         3, strides=strides, padding="same" if strides == 1 else "valid", use_bias=False
     )(m)
