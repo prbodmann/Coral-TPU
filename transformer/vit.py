8c4bda46a9070ce18854ab0c0aefdeecdd2b55ea
prbodmann <prbodmann@gmail.com>
2023-11-07 12:44:12 -0300

    added stuff

--- transformer/vit.py
+++ transformer/vit.py
@@ -22,6 +22,60 @@ def other_gelu(x):
 
 get_custom_objects().update({'other_gelu': Activation(other_gelu)})
 
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, num_heads, d_model, dropout_rate=0.1):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.dropout_rate = dropout_rate
+        
+        assert d_model % num_heads == 0
+        
+        self.depth = d_model // num_heads
+        
+        self.query_dense = tf.keras.layers.Dense(d_model)
+        self.key_dense = tf.keras.layers.Dense(d_model)
+        self.value_dense = tf.keras.layers.Dense(d_model)
+        
+        self.dense = tf.keras.layers.Dense(d_model)
+        
+    def split_heads(self, inputs, batch_size):
+        inputs = tf.reshape(inputs, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(inputs, perm=[0, 2, 1, 3])
+    
+    def scaled_dot_product_attention(self, q, k, v, mask=None):
+        matmul_qk = tf.matmul(q, k, transpose_b=True)
+        dk = tf.cast(tf.shape(k)[-1], tf.float32)
+        scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+        
+        if mask is not None:
+            scaled_attention_logits += (mask * -1e9)
+            
+        attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+        output = tf.matmul(attention_weights, v)
+        
+        return output, attention_weights
+    
+    def call(self, inputs):
+        query, key, value, mask = inputs
+        batch_size = tf.shape(query)[0]
+        
+        query = self.query_dense(query)
+        key = self.key_dense(key)
+        value = self.value_dense(value)
+        
+        query = self.split_heads(query, batch_size)
+        key = self.split_heads(key, batch_size)
+        value = self.split_heads(value, batch_size)
+        
+        scaled_attention, attention_weights = self.scaled_dot_product_attention(query, key, value, mask)
+        
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+        output = self.dense(concat_attention)
+        
+        return output, attention_weights
+
 class CreatePatches( tf.keras.layers.Layer ):
 
   def __init__( self , patch_size,num_patches,input_image_size ):
@@ -132,8 +186,8 @@ def create_vit_classifier(input_shape,
         # Layer normalization 1.
         x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
         # Create a multi-head attention layer.
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        attention_output = MultiHeadAttention(
+            num_heads=num_heads, d_model=projection_dim, dropout=0.1
         )(x1, x1)
         # Skip connection 1.
         x2 = layers.Add()([attention_output, encoded_patches])
