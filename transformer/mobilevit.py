4de27a5f38df0ecfd143986556ce8479ed2c9899
prbodmann <prbodmann@gmail.com>
2023-11-08 14:22:13 -0300

    added stuff

--- transformer/mobilevit.py
+++ transformer/mobilevit.py
@@ -11,8 +11,8 @@ from tensorflow import keras
 patch_size = 4
 b=32
 a=16
-def my_matmul(x,y):
-    temp1= tf.split(y, y.shape[2], axis=1,transpose_b=False)
+def my_matmul(x,y,transpose_b=False):
+    temp1= tf.split(y, y.shape[2], axis=1)
     print(temp1)
     list_tensors = []
 
