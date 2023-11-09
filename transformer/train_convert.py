a9dcb62c71959a0d0cea51d3a318df39fedee690
prbodmann <prbodmann@gmail.com>
2023-11-09 13:13:57 -0300

    added stuff

--- transformer/train_convert.py
+++ transformer/train_convert.py
@@ -180,3 +180,24 @@ tflite_model = converter_quant.convert()
 print("finished converting")
 print(results)
 open(model_name+".tflite", "wb").write(tflite_model)
+interpreter = tf.lite.Interpreter(model_path=model_name+".tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Test model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+
+
+
