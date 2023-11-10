18ba18700182dd54c6625a0bba4daa97a6ad5e05
prbodmann <prbodmann@gmail.com>
2023-11-10 10:49:04 -0300

    added stuff

--- transformer/train_convert.py
+++ transformer/train_convert.py
@@ -28,7 +28,7 @@ optimizer = tfa.optimizers.AdamW(
  
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--model_name', dest = 'model_name',action = 'store',
+parser.add_argument('model_name', dest = 'model_name',action = 'store',
                            default = 'wip_model', required = True)
 parser.add_argument('--training', action = 'store_const', dest = 'training',
                            default = False, required = False,const=True)
